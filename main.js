84d52a93a9e5317701f3a5a01f925985299b7b8b
pepe <andrespaez123@hotmail.com>
2024-02-13 16:42:48 -0500

    commit

--- main.js
+++ main.js
@@ -6,3 +6,10 @@ window.onscroll = function() {
     header.classList.remove('scrolled');
   }
 };
+
+  // Obtiene el año actual
+  var fechaActual = new Date();
+  var anioActual = fechaActual.getFullYear();
+
+  // Muestra el año actual en un elemento HTML
+  document.getElementById("anioActual").innerHTML =  anioActual;
